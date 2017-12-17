ca6cf80eda4adb16d622e7fce5268227cef44c7e
Alex Pask <alex.pask@ladbrokescoral.com>
2017-12-17 17:28:25 +0000

    Integrated all the work together

--- src/main/java/org/awsprog/secretsanta/SantaController.java
+++ src/main/java/org/awsprog/secretsanta/SantaController.java
@@ -1,13 +1,7 @@
 package org.awsprog.secretsanta;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SantaController {
-
-    @GetMapping("/")
-    public String getIndex() {
-        return "index";
-    }
 }
